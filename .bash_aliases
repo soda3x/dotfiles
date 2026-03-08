1748f967d41d35e848a02005e8001bb972d5cc0f
Bradley Newman <bnewman227@gmail.com>
2026-03-08 20:09:12 +1030

    Update dotfiles for CachyOS + KDE

--- .bash_aliases
+++ .bash_aliases
@@ -1,26 +1,6 @@
-alias clear='clear && fastfetch'
-
 # Vim aliases
 alias vim='nvim'
 alias vi='nvim'
-
-# Git aliases
-alias gs='git status'
-alias ga='git add'
-alias gc='git commit'
-alias gco='git checkout'
-alias gb='git branch'
-alias gbr='git branch -a'
-alias gl='git log --oneline --decorate --graph'
-alias gd='git diff'
-alias gcl='git clone'
-alias gp='git pull'
-alias gps='git push'
-alias gpl='git pull --rebase'
-alias gr='git remote -v'
-alias gss='git stash save'
-alias gsp='git stash pop'
-alias gsa='git stash apply'
 
 # Docker aliases
 alias dps='docker ps -a'
@@ -45,3 +25,35 @@ alias dcp='docker-compose pull'
 alias dcps='docker-compose ps'
 alias dcl='docker-compose logs'
 alias dcx='docker-compose exec'
+
+# Bash aliases
+alias c='clear'
+alias t='tmux'
+alias ll='ls -lisa'
+alias l='ll'
+alias h='cd ~'
+alias s='sudo'
+alias sl='s $(history -p !!)' 
+alias rb='source ~/.bashrc'
+alias restart-pipewire='systemctl --user restart pipewire wireplumber'
+
+# Git aliases
+alias gs='git status'
+alias gc='git clone'
+alias gp='git pull'
+alias gps='git push'
+alias gf='git fetch'
+alias gco='git checkout'
+alias grm='git rm'
+alias ga='git add'
+
+# Weather
+alias w='curl wttr.in'
+
+# Tmux aliases
+alias rt='tmux source-file ~/.tmux.conf'
+
+# liquidctl aliases
+
+alias rainbow-on='liquidctl --match smart set sync color spectrum-wave'
+alias rainbow-off='liquidctl --match smart set sync color off'
--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,105 +1,68 @@
-# Set default values for username and gitconfig
-default_name="Bradley Newman"
-default_email="bnewman227@gmail.com"
-default_user="$USER"
-
-if [ "$#" -eq 3 ]; then
-    user="$1"
-    name="$2"
-    email="$3"
-elif [ "$#" -eq 1 ]; then
-    user="$1"
-elif [ "$#" -eq 0 ]; then
-    user="$default_user"
-    name="$default_name"
-    email="$default_email"
-else
-    echo "Usage: $0 [username] [<gitconfig-name> <gitconfig-email>]"
-    exit 1
-fi
+#!/bin/bash
 
 # ===Copy dotfiles===
-curl -LJf https://github.com/soda3x/dotfiles/raw/master/.bash_aliases -o "/home/$user/.bash_aliases"
-curl -LJf https://github.com/soda3x/dotfiles/raw/master/.bashrc -o "/home/$user/.bashrc"
-curl -LJf https://github.com/soda3x/dotfiles/raw/master/.gitconfig -o "/home/$user/.gitconfig"
-curl -LJf https://github.com/soda3x/dotfiles/raw/master/.hushlogin -o "/home/$user/.hushlogin"
-curl -LJf https://github.com/soda3x/dotfiles/raw/master/.tmux.conf -o "/home/$user/.tmux.conf"
+curl -LJf https://github.com/soda3x/dotfiles/raw/master/.bash_aliases -o "/home/$USER/.bash_aliases"
+curl -LJf https://github.com/soda3x/dotfiles/raw/master/.bashrc -o "/home/$USER/.bashrc"
+curl -LJf https://github.com/soda3x/dotfiles/raw/master/.gitconfig -o "/home/$USER/.gitconfig"
+curl -LJf https://github.com/soda3x/dotfiles/raw/master/.hushlogin -o "/home/$USER/.hushlogin"
+curl -LJf https://github.com/soda3x/dotfiles/raw/master/.tmux.conf -o "/home/$USER/.tmux.conf"
+curl -LJf https://github.com/soda3x/dotfiles/raw/master/brad.knsv -o "/home/$USER/brad.knsv"
+curl -LJf https://github.com/soda3x/dotfiles/raw/master/alacritty.toml -o "/home/$USER/.config/alacritty.toml"
 
-# ===Update Gitconfig===
-# I like to use my dotfiles at work too, for work I obviously don't want to use my at-home gitconfig
-# for anyone using my dotfiles, i suppose this will allow you to specify your own gitconfig
-# without editing the file directly
+if [[ "$SHELL" != "/bin/bash" ]]; then
+    echo "Changing default shell to bash..."
+    # Note: This will prompt the user for their password
+    chsh -s /bin/bash
+    echo "Shell changed. You will need to log out and log back in for this to take effect."
+else
+    echo "Bash is already the default shell."
+fi
 
-# Update .gitconfig with provided username and email
-sed -i "s/name = .*/name = $name/" "/home/$user/.gitconfig"
-sed -i "s/email = .*/email = $email/" "/home/$user/.gitconfig"
-
-# ===Install programs===
-
-# Install Nerd Fonts
-git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git
-cd nerd-fonts
-./install.sh
-cd ..
-# Refresh Font Cache
-fc-cache -fv
-
-# Install Cargo
-sudo apt install cargo -y
-
-# Install Fastfetch
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
-sudo apt update
-sudo apt install fastfetch -y
-
-# Install GCC
-sudo apt install gcc -y
-
-# Install Python
-sudo apt install python3 -y
-
-# Install Node
-curl -fsSL https://fnm.vercel.app/install | bash
-fnm use --install-if-missing 20
-
-# Install Neovim + Plugins
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-git clone --depth 1 https://github.com/AstroNvim/template "/home/$user/.config/nvim"
-rm -rf "/home/$user/.config/nvim/.git"
-
-cargo install tree-sitter-cli
-
-sudo apt install ripgrep -y
-
-sudo add-apt-repository ppa:daniel-milde/gdu -y
-sudo apt update
-sudo apt install gdu -y
-
-curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.6/bottom_0.9.6_amd64.deb
-sudo dpkg -i bottom_0.9.6_amd64.deb
-
-# Install Tmux + Tmux Plugin Manager
-sudo apt install tmux -y
-git clone https://github.com/tmux-plugins/tpm "/home/$user/.tmux/plugins/tpm"
-
-# Install Lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-
-# Install Lazydocker
-curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-
-#===Finish===
+# ===Source configs===
 # Source files so you don't need to restart bash
-source "/home/$user/.bash_aliases"
-source "/home/$user/.bashrc"
-source "/home/$user/.gitconfig"
-source "/home/$user/.hushlogin"
-tmux source "/home/$user/.tmux.conf"
+source "/home/$USER/.bash_aliases"
+source "/home/$USER/.bashrc"
+source "/home/$USER/.gitconfig"
+source "/home/$USER/.hushlogin"
 
-# Chown folders created by root
-sudo chown -R $user:$user "/home/$user/.config/nvim/"
+# These will obviously only work if the above source worked
+rb
+rt
+
+echo "Importing KDE Plasma profile..."
+paru -S konsave
+konsave -i "/home/$USER/brad.knsv"
+konsave -a brad
+
+echo "Installing packages..."
+curl -LJf https://github.com/soda3x/dotfiles/raw/master/packages.txt -o "/home/$USER/packages.txt"
+
+REPO_PKGS=()
+AUR_PKGS=()
+PACKAGE_FILE="/home/$USER/packages.txt"
+
+# Read the file, ignoring comments and empty lines
+for pkg in $(grep -E -v '^\s*#|^\s*$' "$PACKAGE_FILE"); do
+    # Check if the package exists in the official repositories
+    if pacman -Si "$pkg" &>/dev/null; then
+        REPO_PKGS+=("$pkg")
+    else
+        AUR_PKGS+=("$pkg")
+    fi
+done
+
+if [[ ${#REPO_PKGS[@]} -gt 0 ]]; then
+    echo "Installing official repository packages: ${REPO_PKGS[*]}"
+    sudo pacman -S --needed --noconfirm "${REPO_PKGS[@]}"
+else
+    echo "No official repository packages to install."
+fi
+
+# Fallback to paru for the remaining packages
+if [[ ${#AUR_PKGS[@]} -gt 0 ]]; then
+    echo "Installing AUR packages using paru: ${AUR_PKGS[*]}"
+    # Note: paru should generally not be run with sudo
+    paru -S --needed --noconfirm "${AUR_PKGS[@]}"
+else
+    echo "No AUR packages to install."
+fi
